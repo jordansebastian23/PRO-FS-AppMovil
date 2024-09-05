82f191d38f4a4ff79e2b41595898335cdc3cc70f
Jordan Navarrete <jordan.navarrete@usm.cl>
2024-09-04 22:31:32 -0400

    Add styles and components

--- proyecto_feria/lib/tabs/archives.dart
+++ proyecto_feria/lib/tabs/archives.dart
@@ -5,6 +5,72 @@ class ArchivesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Card(
+        margin: EdgeInsets.all(20),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.circular(25)
+          ),
+          child: ListTile(
+            title: Text('Archivo Pendiente',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.w600
+            )
+            ),
+            subtitle: Text.rich(TextSpan(children: [
+              TextSpan(
+                text: 'Carga número: 1337\n',
+                style: TextStyle(
+              color: Colors.black,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500
+                )
+              ),
+              TextSpan(
+                text: 'Motivo: Documentos de identidad',
+                style: TextStyle(
+              color: Colors.black,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500
+                )
+              )
+            ])
+            ),
+            trailing: Container(
+              width: 100,
+              height: 100,
+              child: FloatingActionButton(
+                backgroundColor: Color.fromARGB(255, 255, 94, 0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)
+                ),
+                onPressed: () {
+                },
+                child: Text('Ver \n Detalles',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600
+                ),),
+              ),
+            )
+          )
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          
+        },
+        child: Icon(Icons.add,
+        size: 30,
+        color: Colors.white,
+        ),
+        backgroundColor: Color.fromARGB(255, 255, 94, 0),
+      ),
+    );
   }
 }
--- proyecto_feria/lib/tabs/procedures.dart
+++ proyecto_feria/lib/tabs/procedures.dart
@@ -1,10 +1,124 @@
 import 'package:flutter/material.dart';
 
-class ProceduresPage extends StatelessWidget {
+class ProceduresPage extends StatefulWidget {
   const ProceduresPage({super.key});
 
   @override
+  State<ProceduresPage> createState() => _ProceduresPageState();
+}
+
+class _ProceduresPageState extends State<ProceduresPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        title: Text('Historial tramites',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.w600
+        ),),
+      ),
+      body: Container(
+        width: double.maxFinite,
+        height: 40,
+        decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(side: BorderSide(width: 1)),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              left: 10,
+              top: 19,
+              child: SizedBox(
+                width: double.maxFinite,
+                height: 40,
+                
+                child: Text(
+                  'Filtros disponibles',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w600,
+                    height: 0.06,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 270,
+              top: 5,
+              child: SizedBox(
+              width: double.maxFinite,
+              height: 40,
+              child: Container(
+                decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(4),
+              ),
+                child: DropdownButton<String>(
+                  hint: Text('Seleccionar'),
+                  icon: Icon(Icons.arrow_drop_down, color: Colors.black),
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    height: 0.06,
+                  ),
+                  items: <String>['Filter 1', 'Filter 2', 'Filter 3']
+                    .map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                    }).toList(),
+                  onChanged: (_) {
+                    setState(() {
+                      // valor de la variable que se usa para filtrar
+                    });
+                  },
+                  ),
+              ),
+              ),
+              ),
+            
+          ],
+
+        ),
+
+      ),
+      floatingActionButton: Column(
+    mainAxisAlignment: MainAxisAlignment.end,
+    children: [
+      FloatingActionButton(
+        child: Icon(
+          Icons.delete
+        ),
+        onPressed: () {
+          //...
+        },
+        heroTag: null,
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      FloatingActionButton(           
+        child: Icon(
+          Icons.star
+        ),
+        onPressed: () {
+          //...
+        },
+      )
+    ]
+  )
+    );
   }
 }
