3a60efd5cdf4578890a7bbca2eb3b53f569c2589
Jordan Navarrete <jordan.navarrete@usm.cl>
2024-11-03 19:58:55 -0300

    Add colors and styles

---
+++ proyecto_feria/lib/services/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class CustomColor {
+  static Color buttons = Color(0xFF64D1CB);
+  static Color details = Color(0xFF6994D8);
+  static Color background = Color(0xFF272E4B);
+  static Color textPrimary = Color.fromARGB(255, 0, 0, 0);
+  static Color bgCards = Color(0xFFEBEDF0);
+  static Color aditionalImportant = Color(0xFFF4C560);
+}
